75d29ff1d5a4d3752fe25726159b64bec2c30ee6
lautato15 <lautato15@gmail.com>
2023-11-17 12:23:03 -0300

    ItemDetail

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-
-function ItemDetail() {
-  return <div>ItemDetail</div>;
+import ItemCount from "./ItemCount";
+function ItemDetail({ product }) {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 col-md-6">
+          <div className="d-flex justify-content-center align-items-center">
+            <img src={product.image} alt={"Foto de " + product.name} />
+          </div>
+        </div>
+        <div className="col-12 col-md-6">
+          <div className="d-flex justify-content-center align-items-start flex-column fs-4 ">
+            <h2 className="my-5 ">{product.name}</h2>
+            <p>${product.price}</p>
+            <p>
+              <i className="fa-solid fa-star" style={{ color: "#e9cd1c" }}></i>
+              <i className="fa-solid fa-star" style={{ color: "#e9cd1c" }}></i>
+              <i className="fa-solid fa-star" style={{ color: "#e9cd1c" }}></i>
+              <i className="fa-solid fa-star" style={{ color: "#e9cd1c" }}></i>
+              <i className="fa-solid fa-star" style={{ color: "#e9cd1c" }}></i>
+              <span className="ms-4">(3 reviews)</span>
+            </p>
+            <span className="border-gray border-top border-2 w-100"> </span>
+            <p>{product.description}</p>
+            <ItemCount />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ItemDetail;
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-
+import ItemDetail from "./ItemDetail";
 function ItemDetailContainer() {
+  // En ves de hacer otro useEffect con una llamada, traemos los datos por navigate
   const { state } = useLocation();
   const { product } = state;
   console.log(product);
 
   return (
     <div className="d-flex justify-content-center align-items-center text-black fs-2">
-      {product.name}
+      <ItemDetail product={product} />
     </div>
   );
 }
