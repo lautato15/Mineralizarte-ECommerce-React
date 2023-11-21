67392f132511e1a6815c27e6e3801fdb5361bad7
lautato15 <lautato15@gmail.com>
2023-11-21 12:40:56 -0300

    Imgs Updated

---
+++ src/components/home/Slide.jsx
@@ -0,0 +1,46 @@
+import { Carousel, Button } from "react-bootstrap";
+
+import "./Slide.css";
+
+import slide1 from "../../assets/img/slider/slide1.jpg";
+import slide2 from "../../assets/img/slider/slide2.jpg";
+import slide3 from "../../assets/img/slider/slide3.jpg";
+
+function Slide() {
+  return (
+    <Carousel className="w-full	">
+      <Carousel.Item>
+        <img src={slide1} text="First slide" className="w-100" />
+        <Carousel.Caption className="CaptionSlidePosition">
+          <h3 className="SlideText">Ahorra un 70%!</h3>
+          <p className="SlideSubText"> VENTA DE MEDIO AÑO</p>
+          <Button color="white" radius="none" variant="ghost">
+            MÁS VENDIDOS
+          </Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={slide2} text="Second slide" className="w-100" />
+        <Carousel.Caption className="CaptionSlidePosition">
+          <h3 className="SlideText">Lo mejor de lo mejor!</h3>
+          <p className="SlideSubText"> VENTA DE MEDIO AÑO</p>
+          <Button color="white" radius="none" variant="ghost">
+            MÁS VENDIDOS
+          </Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={slide3} text="Third slide" className="w-100" />
+        <Carousel.Caption className="CaptionSlidePosition">
+          <h3 className="SlideText">¡Decile hola a la Novedad!</h3>
+          <p className="SlideSubText"> VENTA DE MEDIO AÑO</p>
+          <Button color="white" radius="none" variant="ghost">
+            MÁS VENDIDOS
+          </Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Slide;
