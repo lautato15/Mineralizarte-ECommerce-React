f4f031f2e59c94aa7579abdcf260492e4ec31cdf
lautato15 <lautato15@gmail.com>
2023-11-22 21:29:26 -0300

    Favourites

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import ItemListContainer from "./ItemListContainer";
 import Categories from "./home/Categories";
+import Favourites from "./home/Favourites";
 import IconIdeas from "./home/IconIdeas";
 import Slide from "./home/Slide";
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
       <div className="d-flex justify-content-center align-items-center text-black fs-2 flex-column bg-black text-white">
         <Slide />
         <Categories />
+        <Favourites />
         <IconIdeas />
       </div>
     </>
---
+++ src/components/home/Favourites.jsx
@@ -0,0 +1,16 @@
+import "./Favourites.css";
+
+function Favourites() {
+  return (
+    <>
+      <div>
+        <div className="container">
+          <h5>DESCUBRE LO DE ESTA SEMANA</h5>
+          <h2>Nuestros Favoritos</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Favourites;
