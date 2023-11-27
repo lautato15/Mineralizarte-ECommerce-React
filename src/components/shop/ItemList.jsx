1c8c2be2f2b56360d2ea395d2ed763d64c8e1a50
lautato15 <lautato15@gmail.com>
2023-11-27 15:29:10 -0300

    Va

--- src/components/shop/ItemList.jsx
+++ src/components/shop/ItemList.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import Item from "../partials/Item";
+
+import HeadBoard from "../partials/HeadBoard";
+import SidebarShop from "../shop/SidebarShop";
+
 function ItemList({ products }) {
   return (
     <>
-      <div className="row">
-        {products.length > 0 &&
-          products.map((p) => {
-            return <Item key={p.id} product={p} />;
-          })}
+      <HeadBoard Text={"Tienda"} />
+      <div className="container">
+        <div className="row py-5">
+          <div className="col-12 col-md-3">
+            <SidebarShop />
+          </div>
+          <div className="col-12 col-md-9">
+            <div className="d-flex w-100 h-100  align-items-center flex-column text-white ">
+              <p className="me-auto">
+                Mostrando los {products.length} resultados
+              </p>
+              <div className="row">
+                {products.length > 0 &&
+                  products.map((p) => {
+                    return <Item key={p.id} product={p} />;
+                  })}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/shop/ItemListContainer.jsx
+++ src/components/shop/ItemListContainer.jsx
@@ -2,9 +2,7 @@ import { useState, useEffect } from "react";
 import "./ItemListContainer.css";
 import ItemList from "./ItemList";
 import { useParams } from "react-router-dom";
-
-import HeadBoard from "../partials/HeadBoard";
-import SidebarShop from "../shop/SidebarShop";
+import axios from "axios";
 
 function ItemListContainer() {
   const [products, setProducts] = useState([]);
@@ -32,23 +30,8 @@ function ItemListContainer() {
     products.length > 0 && (
       <>
         <div className="bg-dark">
-          <HeadBoard Text={"Tienda"} />
-          <div className="container">
-            <div className="row py-5">
-              <div className="col-12 col-md-3">
-                <SidebarShop />
-              </div>
-              <div className="col-12 col-md-9">
-                <div className="d-flex w-100 h-100  align-items-center flex-column text-white ">
-                  <p className="me-auto">
-                    Mostrando los {products.length} resultados
-                  </p>
-                  {category && <h2>{category.name}</h2>}
-                  <ItemList products={products} />
-                </div>
-              </div>
-            </div>
-          </div>
+          {category && <h2>{category.name}</h2>}
+          <ItemList products={products} />
         </div>
       </>
     )
