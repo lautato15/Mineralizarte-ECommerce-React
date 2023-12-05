5710e8f92ac18f8cbc9dbc536f261e1d81e070a3
lautato15 <lautato15@gmail.com>
2023-12-05 16:59:53 -0300

    Update Cart.jsx

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,138 +1,108 @@
 import { Link } from "react-router-dom";
 import HeadBoard from "../partials/HeadBoard";
+import InputCart from "../partials/CartWidget/InputCart";
 
 function Cart() {
+  const classTd = "y";
   return (
     <>
-      <div className="bg-dark">
+      <div className="bg-dark FontLato">
         <HeadBoard Text={"Carrito"} />
         <div className="container">
           <div className="row py-5">
-            <div className="col-12 col-md-8">
-              <table class=" table" cellspacing="0">
-                <thead>
+            <div className="col-12 col-md-8 ">
+              <table class="table" cellspacing="0">
+                <thead className="">
                   <tr>
-                    <th>Product</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Subtotal</th>
-                    <th>&nbsp;</th>
+                    <th className={classTd}>Producto</th>
+                    <th className={classTd}>Precio</th>
+                    <th className={classTd}>Cantidad</th>
+                    <th className={classTd}>Subtotal</th>
+                    <th className={classTd}>&nbsp;</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <td>
+                    <td className="align-middle bg-secondary">
                       <a href="shop-details.html">
-                        <img src="media/product/3.jpg" class="" alt="" />
-                      </a>
-                      <div>
-                        <a href="shop-details.html">Twin Hoops</a>
-                      </div>
-                    </td>
-                    <td>
-                      <span>$150.00</span>
-                    </td>
-                    <td>
-                      <div>
-                        <button type="button">-</button>
-                        <input
-                          type="number"
-                          step="1"
-                          min="0"
-                          max=""
-                          name="quantity"
-                          value="2"
-                          title="Qty"
-                          size="4"
-                          placeholder=""
-                          inputmode="numeric"
-                          autocomplete="off"
+                        <img
+                          src="https://caketheme.com/html/mojuri/media/product/6.jpg"
+                          class="border border-black border-2"
+                          alt=""
+                          style={{
+                            width: "70px",
+                            height: "70px",
+                          }}
                         />
-                        <button type="button">+</button>
-                      </div>
-                    </td>
-                    <td>
-                      <span>$300.00</span>
-                    </td>
-                    <td>
-                      <a href="#" class="remove">
-                        ×
+                      </a>
+
+                      <a
+                        href="shop-details.html"
+                        className="text-decoration-none text-black fw-bold ms-3"
+                      >
+                        Twin Hoops
                       </a>
                     </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <a href="shop-details.html">
-                        <img src="media/product/1.jpg" class="" alt="" />
-                      </a>
-                      <div class="product-name">
-                        <a href="shop-details.html">Medium Flat Hoops</a>
-                      </div>
+                    <td className="align-middle bg-secondary">
+                      <span className="m-0 ">$150.00</span>
                     </td>
-                    <td>
-                      <span>$180.00</span>
-                    </td>
-                    <td>
+                    <td className="align-middle bg-secondary">
                       <div>
-                        <button type="button">-</button>
-                        <input
-                          type="number"
-                          step="1"
-                          min="0"
-                          max=""
-                          name="quantity"
-                          value="1"
-                          title="Qty"
-                          size="4"
-                          placeholder=""
-                          inputmode="numeric"
-                          autocomplete="off"
-                        />
-                        <button type="button" class="plus">
+                        <button
+                          type="button"
+                          className="bg-black border-0 text-white  px-2 h-100"
+                        >
+                          -
+                        </button>
+                        <InputCart id="{}" counter="{}" />
+                        <button
+                          type="button"
+                          className="bg-black border-0 text-white  px-2 h-100"
+                        >
                           +
                         </button>
                       </div>
                     </td>
-                    <td>
-                      <span class="price">$180.00</span>
+                    <td className="align-middle bg-secondary">
+                      <span className="">$300.00</span>
                     </td>
-                    <td class="product-remove">
-                      <a href="#" class="remove">
+                    <td className="align-middle bg-secondary">
+                      <a href="#" class="text-decoration-none text-black">
                         ×
                       </a>
                     </td>
                   </tr>
+
                   <tr>
-                    <td colspan="6" class="actions">
-                      <div class="bottom-cart">
-                        <div class="coupon">
+                    <td colspan="6" class="bg-secondary align-middle border-0">
+                      <div class="d-flex align-middle">
+                        <div class="cupon me-auto d-flex align-items-center">
                           <input
                             type="text"
                             name="coupon_code"
-                            class="input-text"
+                            class="h-75 border-0 h-100 ps-2"
                             id="coupon-code"
                             value=""
-                            placeholder="Coupon code"
+                            placeholder="Codigo de Cupon"
                           />
+
                           <button
                             type="submit"
                             name="apply_coupon"
-                            class="button"
+                            class="btn rounded-0 bg-black text-white ms-2 my-0 border-0 h-100"
                             value="Apply coupon"
                           >
-                            Apply coupon
+                            Aplicar Cupon
                           </button>
                         </div>
-                        <h2>
-                          <a href="shop-grid-left.html">Continue Shopping</a>
-                        </h2>
+
                         <button
                           type="submit"
                           name="update_cart"
-                          class="button"
+                          class="btn rounded-0 bg-black text-white ms-2"
                           value="Update cart"
                         >
-                          Update cart
+                          Continuar Comprando
                         </button>
                       </div>
                     </td>
@@ -140,17 +110,20 @@ function Cart() {
                 </tbody>
               </table>
             </div>
+
             <div className="col-12 col-md-4 ">
               {/* SUBTOTAL */}
+              <h2 className="fs-5 p-4 fw-bold bg-secondary">
+                Total del Carrito
+              </h2>
               <div className="bg-secondary p-4">
-                <h2 className="fs-5 mb-5 ">Total del Carrito</h2>
-                <div className="d-flex">
-                  <p className="w-50">SubTotal</p>
+                <div className="d-flex pt-3">
+                  <p className="w-50 fw-bold">SubTotal</p>
                   <p className="w-50">$ 480.00</p>
                 </div>
                 {/* ENVIO */}
                 <div className="d-flex my-4">
-                  <p className="w-50">Envío</p>
+                  <p className="w-50 fw-bold">Envío</p>
                   <div className="w-50">
                     <div>
                       <input
@@ -176,12 +149,12 @@ function Cart() {
                 </div>
                 {/* TOTAL */}
                 <div className="d-flex">
-                  <p className="w-50">Total</p>
+                  <p className="w-50 fw-bold">Total</p>
                   <p className="w-50">$ 480.00</p>
                 </div>
                 <Link
                   to="/checkout"
-                  className="text-decoration-none text-black BgYellow d-block w-100 ms-auto me-auto text-center py-2"
+                  className="text-decoration-none text-black BgYellow d-block w-100 ms-auto me-auto text-center py-2 fw-bold"
                 >
                   Ir a Pagar!
                 </Link>
